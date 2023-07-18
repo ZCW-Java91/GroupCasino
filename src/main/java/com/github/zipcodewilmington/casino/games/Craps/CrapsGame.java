7e657d35d51df751d86be463f17d368b18bed971
Chanelle <chanelle@Zipcoders-MacBook-Pro-103.local>
2023-07-18 17:59:21 -0400

    Added test classes

--- src/main/java/com/github/zipcodewilmington/casino/games/Craps/CrapsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Craps/CrapsGame.java
@@ -1,4 +1,17 @@
 package com.github.zipcodewilmington.casino.games.Craps;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class CrapsGame {
+    private static Random random = new Random();
+    private static Scanner scanner = new Scanner(System.in);
+    public static int rollDice() {
+        return random.nextInt(6) + 1;
+    }
+
+
+    public String playGame() {
+        return null;
+    }
 }
---
+++ src/test/java/com/github/zipcodewilmington/CrapsTests/CrapsPlayerTest.java
@@ -0,0 +1,7 @@
+package com.github.zipcodewilmington.CrapsTests;
+
+public class CrapsPlayerTest {
+
+}
+
+

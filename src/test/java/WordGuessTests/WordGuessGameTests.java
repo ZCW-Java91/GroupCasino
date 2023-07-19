b9130f1a9382887cd942588243ac8d81cc272573
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-07-18 20:06:48 -0400

    tests started

---
+++ src/test/java/WordGuessTests/WordGuessGameTests.java
@@ -0,0 +1,29 @@
+package WordGuessTests;
+
+import com.github.zipcodewilmington.casino.games.WordGuess.WordGuessGame;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WordGuessGameTests {
+
+    @Before
+
+
+
+
+    @Test
+    public void setAmountOfGuessesTest(){
+        //Given
+        WordGuessGame wordGuess = new WordGuessGame();
+        Integer expected = 6;
+        String wordToGuess = "Banana";
+
+        //When
+
+
+
+        //Then
+    }
+
+}

88bba807dc02de5002f62baafa989e8e4110f4dd
maggie <snydermag@gmail.com>
2023-07-19 09:39:34 -0400

    added some test cases

---
+++ src/test/java/com/github/zipcodewilmington/slotstest/SlotsPlayerTest.java
@@ -0,0 +1,68 @@
+package com.github.zipcodewilmington.slotstest;
+
+import com.github.zipcodewilmington.casino.games.slots.SlotsPlayer;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SlotsPlayerTest {
+
+    @Test
+    public void setBalanceTest(){
+        // Given
+        int expected = 1000;
+
+        // When
+        SlotsPlayer player = new SlotsPlayer();
+        player.setBalance(1000);
+
+        int actual = player.getBalance();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getBalanceTest(){
+        // Given
+        int expected = 1000;
+
+        // When
+        SlotsPlayer player = new SlotsPlayer();
+        player.setBalance(1000);
+
+        int actual = player.getBalance();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getBetTest(){
+        // Given
+        int expected = 5;
+
+        // When
+        SlotsPlayer player = new SlotsPlayer();
+        player.setBet(5);
+
+        int actual = player.getBet();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setBetTest(){
+        // Given
+        int expected = 5;
+
+        // When
+        SlotsPlayer player = new SlotsPlayer();
+        player.setBet(5);
+
+        int actual = player.getBet();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+}

116de5b56a75519a91dd78a480b06f6c4a7c371f
Brent <BrentC0328@gmail.com>
2023-07-18 22:04:10 -0400

    testing started

--- src/test/java/WordGuessTests/WordGuessGameTests.java
+++ src/test/java/WordGuessTests/WordGuessGameTests.java
@@ -7,23 +7,42 @@ import org.junit.Test;
 
 public class WordGuessGameTests {
 
-    @Before
-
-
 
 
     @Test
-    public void setAmountOfGuessesTest(){
+    public void getAmountOfGuessesTest(){
         //Given
         WordGuessGame wordGuess = new WordGuessGame();
         Integer expected = 6;
         String wordToGuess = "Banana";
 
         //When
-
-
+        Integer actual = wordGuess.getAmountOfGuesses(wordToGuess);
 
         //Then
+        Assert.assertEquals(expected, actual);
     }
+
+    public void setWordTest(){
+        //given
+        WordGuessGame wordGuess = new WordGuessGame();
+        char[] expected = new char[]{'p','u','r','p','l','e'};
+        String wordString = "Purple";
+
+        //when
+        wordGuess.setWord(purple);
+        char[] actual = wordGuess.getWord();
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+
+
+
+
+
+
+
 
 }

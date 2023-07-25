cd189d8a662a375d0455c9368ff63c77b6da232e
Angela Kim <aangelakkim@gmail.com>
2023-07-24 20:38:22 -0400

    casino

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -20,102 +20,107 @@ import static com.github.zipcodewilmington.casino.games.slots.SlotsPlayer.prompt
 public class SlotsGame implements GameInterface {
 
     String[] wordList = {"*STAR*", "*LUCK*", "*CASH*", "*PRAY*", "*HOPE*"};
-    SlotsPlayer sp = new SlotsPlayer();
+    SlotsPlayer sp; //instance of player
+    private final IOConsole console = new IOConsole(AnsiColor.CYAN);
+    int bet;
 
-
-//    just temp to check if code works
-
-
-
-
-    public void run() {
-        int numberOfTries = 0;
-        boolean hasMatch = false;
-
-
-        displayInstructions();
-
-        do {
-            promptUserToStart();
-            String[] wordList = spin();
-            printResult(wordList);
-            numberOfTries++;
-
-            if (checkMatch(wordList)) {
-                hasMatch = true;
-                break;
-            }
-
-            if (numberOfTries < 3) {
-                System.out.println("Press Enter to spin again!");
-            }
-
-
-        }
-        while (numberOfTries < 3);
-
-        if (hasMatch) {
-            updateAccount();
-        } else {
-            quit();
-        }
+    public SlotsGame(SlotsPlayer splayer) {
+        sp = splayer;
     }
 
-        @Override
-        public void displayInstructions () {
-            System.out.println(" ***************************************");
-            System.out.println(" ---------------------------------------");
-            System.out.println(" { Welcome to the Slot Machine Game!!! } ");
-            System.out.println(" ---------------------------------------");
-            System.out.println(" ***************************************\n");
-            System.out.println("     Type 'PULL' to crank the lever!\n ");
-            System.out.print(">>>   ");
+    public SlotsGame() {
+    }
+
+    public void run() {
+
+        displayInstructions();
+        placeBet();
+        winOrLose();
+
+    }
+
+    @Override
+    public void displayInstructions () {
+        System.out.println(" ***************************************");
+        System.out.println(" ---------------------------------------");
+        System.out.println(" { Welcome to the Slot Machine Game!!! } ");
+        System.out.println(" ---------------------------------------");
+        System.out.println(" ***************************************\n");
+        System.out.print(">>>   ");
+        System.out.println("  Hi " + sp.getPlayerAccount().getUserName() + ", your current balance is: $" + sp.getAccount().getBalance());
+    }
+
+    public int placeBet() {
+        return bet = console.getIntegerInput("Place your bet: ");
+    }
+
+    public String[] spin () {
+        Random random = new Random();
+        for (int i = 0; i < wordList.length; i++) {
+            int randomIndex = random.nextInt(wordList.length);
+            wordList[i] = wordList[randomIndex];
         }
+        return wordList;
+    }
 
+    public void askUserToPullMsg () {
+        System.out.println("     Type 'PULL' to crank the lever!\n ");
+    }
 
-        public String[] spin () {
-            Random random = new Random();
-            for (int i = 0; i < wordList.length; i++) {
-                int randomIndex = random.nextInt(wordList.length);
-                wordList[i] = wordList[randomIndex];
-            }
-            return wordList;
-        }
+    public boolean checkMatch (String[]wordList){
 
+        boolean indexCheck= wordList[0].equals(wordList[1]) && wordList[0].equals(wordList[2]);
+        return  indexCheck;
+    }
 
-        public void printResult (String[]wordList){
+    public boolean winOrLose () {
+        int tries = 3;
+
+        while (tries > 0) {
+            askUserToPullMsg();
+            promptUserToStart();
+            spin();
             System.out.println(wordList[0] + " " + wordList[1] + " " + wordList[2]);
+            System.out.println("\n");
+
             if (checkMatch(wordList)) {
-                System.out.println("Congratulations! You have a match!\n");
-            } else {
-                System.out.println("Better luck next time. No match.\n");
+                System.out.println("Congratulations! You have a match! You have doubled your bet!\n");
+                sp.getPlayerAccount().addBalance(bet * 2);
+                sp.getPlayerAccount().setBalance(sp.getPlayerAccount().getBalance());
+                System.out.println("Your balance now is: " + sp.getPlayerAccount().getBalance() + "\n");
+                return true;
+
             }
+            tries --;
         }
 
+        noMatch();
+        return false;
 
-        public boolean checkMatch (String[]wordList){
+    }
 
-            return wordList[0].equals(wordList[1]) && wordList[0].equals(wordList[2]);
+    public void noMatch() {
+        System.out.println("Better luck next time. No match. You lost your bet!\n");
+        sp.getPlayerAccount().withdrawBalance(bet);
+        sp.getPlayerAccount().setBalance(sp.getPlayerAccount().getBalance());
+        System.out.println("Your balance now is: " + sp.getPlayerAccount().getBalance() + "\n");
+
+    }
+
+
+
+    @Override
+    public void quit () {
+
+        System.out.println("Thank you for playing! Press 1 to replay or 2 to return to the casino lobby.");
+        Scanner console = new Scanner(System.in);
+        int choice = console.nextInt();
+        if (choice == 1) {
+            run();
+        } else if (choice == 2) {
+            //back to main menu when i comment out this classes main method
         }
-
-
-        public void updateAccount () {
-            System.out.println("BLOOOP.\n");
-
-        }
-
-        @Override
-        public void quit () {
-
-            System.out.println("Thank you for playing! Press 1 to replay or 2 to return to the casino lobby.");
-            Scanner console = new Scanner(System.in);
-            int choice = console.nextInt();
-            if (choice == 1) {
-                run();
-            } else if (choice == 2) {
-                //back to main menu when i comment out this classes main method
-            }
-        }
+    }
 
     @Override
     public void add(Player player) {
